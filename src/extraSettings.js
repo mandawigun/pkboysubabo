18cce3fba0d42300715587eeeec0bee8a711001a
mandawigun <seunghyojeong934@gmail.com>
2021-03-20 05:06:30 +0900

    pkboysubabo Ver.1.0

--- src/extraSettings.js
+++ src/extraSettings.js
@@ -38,7 +38,7 @@ let clickChangeFruit = true;
 let adLink = 
 
 // 修改网页标题：将 "合成大西瓜" 进行替换
-document.getElementsByTagName("title")[0].innerText = '合成大西瓜';
+document.getElementsByTagName("title")[0].innerText = '피케이보이님안녕하세요';
 
 // 开启选分弹窗：将 false 改为 true
 let selectModal = false;
